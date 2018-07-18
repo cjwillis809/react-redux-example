24ab693b416210e051e48e956843c707de724490
cjwillis809 <cjwillis809@gmail.com>
2018-07-18 14:49:26 -0500

    Modified action test

--- test/actions.test.ts
+++ test/actions.test.ts
@@ -5,7 +5,7 @@ describe('actions', () => {
     it('should create an action to set the category filter', () => {
         const filterId = 7;
         const expectedAction = {
-            filterId: filterId,
+            filterId,
             type: FilterActionTypes.SET_CATEGORY_FILTER
         }
         expect(setCategoryFilter(filterId)).toEqual(expectedAction)
