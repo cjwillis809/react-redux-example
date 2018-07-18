d6b1538da677c99e96fc20c53207a3d38c59ec2d
cjwillis809 <cjwillis809@gmail.com>
2018-07-18 12:48:27 -0500

    Added more filter reducer tests

--- test/reducers.test.ts
+++ test/reducers.test.ts
@@ -7,4 +7,23 @@ describe('reducers', () => {
             categoryFilter: -1
         })
     });
+
+    it('FilterReducer - should handle SET_CATEGORY_FILTER', () => {
+        const expectedState = {
+            categoryFilter: 1
+        }
+        const expectedState2 = {
+            categoryFilter: 2
+        }
+
+        expect(filterReducer(undefined, {
+            filterId: 1,
+            type: FilterActionTypes.SET_CATEGORY_FILTER
+        })).toEqual(expectedState)
+
+        expect(filterReducer(expectedState, {
+            filterId: 2,
+            type: FilterActionTypes.SET_CATEGORY_FILTER
+        })).toEqual(expectedState2)
+    });
 })
